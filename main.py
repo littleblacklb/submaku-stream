ed8349e40f98adca0ae674d10072e7af1c19c09a
littleblack LB <lbk@kano.cat>
2025-01-20 15:28:54 +0800

    :wrench: feat: handle termination signals

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import asyncio
-import atexit
-import signal
 import sys
 import time
 
@@ -24,24 +22,27 @@ async def main():
         logger.warning("Debug mode is enabled.")
         logger.remove()
         logger.add(sys.stdout, level="DEBUG")
-    stream_url = (await network.get_stream_urls())[0]
-    logger.info(f"Stream URL: {stream_url}")
+    try:
+        stream_url = (await network.get_stream_urls())[0]
+        logger.info(f"Stream URL: {stream_url}")
 
-    logger.info("Loading model...")
-    t0_perf = time.time()
-    model = whispers.LocalWhisper("base").model
-    delta_t_perf = (time.time() - t0_perf) * 1000
-    logger.success(f"Model loaded. {delta_t_perf:.2f}ms")
-    while 1:
-        try:
-            async for audio_chunk in process_audio_segments(stream_url, chunk_duration=config.segment_time_length):
-                asyncio.create_task(danmaku_worker(model, audio_chunk))
-        except ffmpeg.Error as e:
-            logger.error("Error occurred while processing the audio stream:", e)
-            logger.error(e.stderr.decode())
-        except EndOfStream:
-            logger.warning("Stream ended. Program exits")
-            sys.exit(0)
+        logger.info("Loading model...")
+        t0_perf = time.time()
+        model = whispers.LocalWhisper("turbo").model
+        delta_t_perf = (time.time() - t0_perf) * 1000
+        logger.success(f"Model loaded. {delta_t_perf:.2f}ms")
+        while 1:
+            try:
+                async for audio_chunk in process_audio_segments(stream_url, chunk_duration=config.segment_time_length):
+                    asyncio.create_task(danmaku_worker(model, audio_chunk))
+            except ffmpeg.Error as e:
+                logger.error("Error occurred while processing the audio stream:", e)
+                logger.error(e.stderr.decode())
+            except EndOfStream:
+                logger.warning("Stream ended. Program exits")
+                sys.exit(0)
+    except asyncio.CancelledError:
+        logger.warning("CancelledError is caught, exiting the program.")
 
 
 async def danmaku_worker(model: Whisper, audio_array: np.ndarray):
